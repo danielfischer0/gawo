a4ff71d8b3f21450b0336c78f3206eeeb01cfa51
danielfischer0 <interes.mir2014@yandex.ru>
2017-05-13 15:39:05 +0200

    Update code.php

--- code.php
+++ code.php
@@ -1,9 +1,32 @@
 <?php
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    /*if($_SERVER['REQUEST_METHOD'] == 'POST') {
       $gebe = $_POST('gebe');
       $nehme = $_POST('nehme');
       echo $gebe;
       echo $nehme;
-      //$massiv = array($gebe => $nehme);
-}
+      $massiv = array($gebe => $nehme);
+}*/
+echo "<html>
+<head>
+<meta charset=\"utf-8\">
+</head>
+<body>
+<h1>GaWo - Tausch</h1>
+Hier ist möglich miteinander GaWo - veranstaltungen zu tauschen.
+<br>
+<form action=\"/code.php\" method=\"post\">
+  <input type=\"text\" name=\"gebe\" value=\"Möchte abgeben\">
+  <input type=\"text\" name=\"nehme\" value=\"Möchte haben\">
+  <!--<button type=\"button\" name=\"button\">Laden</button>-->
+  <input type=\"submit\" value=\"Laden\">
+</form>
+</body>
+</html>
+";
+//if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $gebe = $_POST('gebe');
+  $nehme = $_POST('nehme');
+  echo $gebe;
+  echo $nehme;
+  $massiv = array($gebe => $nehme);//}
 ?>
